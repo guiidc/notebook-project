90fb0fd0c8ec43baa3df4e012216ce4560a5acd0
Costa <eusoude74@gmail.com>
2022-07-20 16:43:00 -0300

    html e css melhorado

---
+++ script.js
@@ -0,0 +1,13 @@
+function criarContato(){
+    const criaContato = document.querySelector(elemento)
+    // modalcria.classList.add("none")
+    console.log(el)
+}  
+
+
+
+const criaContato = document.querySelector("#button-criar-contato")
+criaContato.addEventListener('click', criarContato);
+
+
+
