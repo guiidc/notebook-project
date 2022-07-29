da967c8f735c3eb3705ba26131764a309df64279
Costa <eusoude74@gmail.com>
2022-07-29 19:04:26 -0300

    projeto refaturado

--- script.js
+++ script.js
@@ -22,9 +22,8 @@ function mostrarModalCriarCont() {
 
 let contatoASerRemovido = null;
 
-function mostrarModalRemover(confirmacao) {
-
-    const target = confirmacao.target; // SEGURA O BOTAO REMOVER QUE DISPAROU O EVENTO
+function mostrarModalRemover(evento) {
+    const target = evento.target; // SEGURA O BOTAO REMOVER QUE DISPAROU O EVENTO
     const containerBotoesAcao = target.parentElement;
     const cardContact = containerBotoesAcao.parentElement
     contatoASerRemovido = cardContact
@@ -47,10 +46,19 @@ function fecharModalRemocao() {
 }
 
 function removerContatoSelecionado() {
-
     contatoASerRemovido.remove()
-
     fecharModalRemocao()
+}
+
+function criaElemento(elemento, classes, texto) {
+    const novoElemento = document.createElement(elemento);
+    for (let i = 0; i < classes.length; i++) {
+        novoElemento.classList.add(classes[i]);
+    }
+    if (texto !== undefined) {
+        novoElemento.innerText = texto
+    }
+    return novoElemento
 }
 
 function salvaContato() {
@@ -58,46 +66,60 @@ function salvaContato() {
     const contato = inputContato.value
     const grupo = inputGrupo.value
 
-    const ContainerCardContato = document.createElement('div')
-    ContainerCardContato.classList.add('container-card-contato')
+    // const ContainerCardContato = document.createElement('div')
+    // ContainerCardContato.classList.add('container-card-contato')
+    const ContainerCardContato = criaElemento('div', ['container-card-contato'])
 
-    const divNome = document.createElement('div')
-    divNome.classList.add('flex-center')
-    const spanNome = document.createElement('span')
-    spanNome.classList.add('fonte-card')
-    spanNome.innerText = nome
+    // const divNome = document.createElement('div')
+    // divNome.classList.add('flex-center')
+    const divNome = criaElemento('div', ['flex-center'])
+
+    // const spanNome = document.createElement('span')
+    // spanNome.classList.add('fonte-card')
+    // spanNome.innerText = nome
+    const spanNome = criaElemento('span', ['fonte-card'], nome)
+
     divNome.appendChild(spanNome)
     ContainerCardContato.appendChild(divNome)
 
-    const divContato = document.createElement('div')
-    divContato.classList.add('flex-center')
-    const spanContato = document.createElement('span')
-    spanContato.classList.add('fonte-card')
-    spanContato.innerText = contato
+    // const divContato = document.createElement('div')
+    // divContato.classList.add('flex-center')
+    const divContato = criaElemento('div', ['flex-center'])
+
+    // const spanContato = document.createElement('span')
+    // spanContato.classList.add('fonte-card')
+    // spanContato.innerText = contato
+    const spanContato = criaElemento('span', ['fonte-card'], contato)
     divContato.appendChild(spanContato)
     ContainerCardContato.appendChild(divContato)
 
-    const divGupo = document.createElement('div')
-    divGupo.classList.add('flex-center')
-    const spanGrupo = document.createElement('span')
-    spanGrupo.classList.add('fonte-card')
-    spanGrupo.innerText = grupo
-    divGupo.appendChild(spanGrupo)
-    ContainerCardContato.appendChild(divGupo)
+    // const divGrupo = document.createElement('div')
+    // divGupo.classList.add('flex-center')
+    const divGrupo = criaElemento('div', ['flex-center'])
 
-    const containerBtnAcao = document.createElement('div')
-    containerBtnAcao.classList.add('container-btn-acao')
-    const btnAcaoEditar = document.createElement('button')
-    btnAcaoEditar.classList.add('btn-acao', 'bg-azul', 'pointer')
-    btnAcaoEditar.innerText = 'EDITAR'
+    // const spanGrupo = document.createElement('span')
+    // spanGrupo.classList.add('fonte-card')
+    // spanGrupo.innerText = grupo
+    const spanGrupo = criaElemento('span', ['fonte-card'], grupo)
+    divGrupo.appendChild(spanGrupo)
+    ContainerCardContato.appendChild(divGrupo)
+
+    // const containerBtnAcao = document.createElement('div')
+    // containerBtnAcao.classList.add('container-btn-acao')
+    const containerBtnAcao = criaElemento('div', ['container-btn-acao'])
+
+    // const btnAcaoEditar = document.createElement('button')
+    // btnAcaoEditar.classList.add('btn-acao', 'bg-azul', 'pointer')
+    // btnAcaoEditar.innerText = 'EDITAR'
+    const btnAcaoEditar = criaElemento('button', ['btn-acao', 'bg-azul', 'pointer'], 'EDITAR')
     btnAcaoEditar.addEventListener('click', mostrarModalCriarCont)
     containerBtnAcao.appendChild(btnAcaoEditar)
 
-    const btnAcaoRemover = document.createElement('button')
-    btnAcaoRemover.classList.add('btn-acao', 'bg-vermelho', 'pointer')
-    btnAcaoRemover.innerText = 'REMOVER'
+    // const btnAcaoRemover = document.createElement('button')
+    // btnAcaoRemover.classList.add('btn-acao', 'bg-vermelho', 'pointer')
+    // btnAcaoRemover.innerText = 'REMOVER'
+    const btnAcaoRemover = criaElemento('button', ['btn-acao', 'bg-vermelho', 'pointer'], 'REMOVER')
     btnAcaoRemover.addEventListener('click', mostrarModalRemover)
-
     containerBtnAcao.appendChild(btnAcaoRemover)
     ContainerCardContato.appendChild(containerBtnAcao)
 
